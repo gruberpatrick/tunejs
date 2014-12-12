2306f4c28f6d580f2a89f473cdee8f89b2dc7554
Patrick Gruber <patrick19gruber93@gmail.com>
2014-12-12 21:42:43 +0100

    modifications, added some Files

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 TuneJS
 ======
 
-Currently working.
-Meanwhile: http://gruberpatrick.tumblr.com/post/104938296340/starting-a-new-open-source-project
+A WebSocket controlled MusicPlayer, using a simple Protocol to manage Playback and Library remotely.
+
+Read why here: http://gruberpatrick.tumblr.com/post/104938296340/starting-a-new-open-source-project
--- app.js
+++ app.js
@@ -1,14 +1,27 @@
+DB = require('./lib/db.lib');
 var Log = require('./lib/log.lib');
-var DB = require('./lib/db.lib');
 var Player = require('./lib/player.lib');
+var Websocket = require('./lib/socket.lib');
+var Tools = require('./lib/tools.lib');
+var Messages = require('./lib/messages.lib');
+var Controller = require('./lib/controller.lib');
 
+// initialize DB and Player
 DB.DB();
-DB.loadFolder('/media/patrick/swap/music/', function(lLoaded, lTotal){ console.log(lLoaded + " - " + lTotal); });
-
-for(var i in DB.oDB.oSongs){
-	Player.addSong(i);
-}
-
+Player.createPlaylist("SHUFFLE");
 Player.play(function(oSong){
-	console.log(oSong);
+	Log.log("Now playing: \"" + oSong.sTitle + " << " + oSong.sArtist + "\"");
+	Websocket.broadcast(Messages.createSongChangedMessage(oSong));
+});
+
+// open Socket connection
+Websocket.startSocket(1234, function(sMessage, oWS){
+
+	var oMessage = JSON.parse(sMessage);
+
+	if(oMessage.type == "PlayerCommand")
+		Controller.parsePlayerCommand(oMessage.content);
+	else if(oMessage.type == "SongRequest")
+		Controller.parseSongRequest(oMessage.content);
+
 });
