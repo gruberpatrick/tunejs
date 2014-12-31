68ef814a4feb5f31629141d0a0291090aaa69a77
Patrick Gruber <patrick19gruber93@gmail.com>
2014-12-31 15:26:40 +0100

    modifications

--- themes/.libs/full.js
+++ themes/.libs/full.js
@@ -1,6 +1,4 @@
-var oCurrentSong = {};
-var oPlayer = {};
-var bPlaying = true;
+var oMediaScope = null;
 
 var FullTmpl = {
 
@@ -60,58 +58,95 @@ $("#fullscreenbutton").click(function(){
 	FullTmpl.toggleFullscreen();
 });
 
+// -----------------------------------------------------------------------------
 // Media APP Angular Code
+//
 (function(){
 
 	var app = angular.module("mediaApp", []);
-	app.controller("MediaControl", function(){
+	app.controller("MediaControl", function($scope){
 
-		this.setPlay = function(bPlay){
-			bPlaying = bPlay;
-		};
+		// Angular Control
+		oMediaScope = $scope;
+		$scope.bPlay = true;
+		$scope.oCurrentSong = {};
+		$scope.oPlayer = {};
+		$scope.lPlayCounter = 0;
+		$scope.oTimeout = null;
+		$scope.lPercentage = 0;
 
-		this.isPlaying = function(){
-			return bPlaying;
-		};
-
-		var oClient = new WebSocket("ws://localhost:1234");
-		oClient.onopen = function(){
-			oClient.send(JSON.stringify({"type":"SongRequest","content":""}));
-			$("#next").click(function(){
-				oClient.send(JSON.stringify({"type":"PlayerCommand","content":"next"}));
-			});
-			$("#prev").click(function(){
-				oClient.send(JSON.stringify({"type":"PlayerCommand","content":"prev"}));
-			});
-			$("#play").click(function(){
-				if(bPlaying){
-					oClient.send(JSON.stringify({"type":"PlayerCommand","content":"pause"}));
-				}else{
-					oClient.send(JSON.stringify({"type":"PlayerCommand","content":"play"}));
-				}
+		$scope.setPlay = function(bPlay){
+			$scope.$apply(function(){
+				$scope.bPlay = bPlay;
 			});
 		};
 
-		oClient.onmessage = function(sMessage){
-			var oMessage = JSON.parse(sMessage.data);
-			if(oMessage.type == "SongChanged" || oMessage.type == "SongResponse" || oMessage.type == "PlayerStatusChanged"){
-				oCurrentSong = oMessage.content;
-				$("#songtitle").html(oMessage.content.sTitle);
-				oMessage.content.sAlbum != "" ? $("#songalbum").html(oMessage.content.sAlbum) : $("#songalbum").html(oMessage.content.sArtist + " <unknown>");
-				$("#songartist").html(oMessage.content.sArtist);
-				if(oMessage.content.lLength > 0){
-					$("#songcomplete").html(Math.floor(oMessage.content.lLength / 60) + ":" + (oMessage.content.lLength % 60 < 10 ? "0" + oMessage.content.lLength % 60 : oMessage.content.lLength % 60));
-				}
-				if(typeof oMessage.player != "undefined"){
-					oPlayer = oMessage.player;
-					bPlaying = oMessage.player.bPlaying;
-					if(bPlaying)
-						$("#play").addClass("pause");
-					else
-						$("#play").removeClass("pause");
-				}
-			}
-		}
+		$scope.setCurrentSong = function(oSong){
+			$scope.$apply(function(){
+				$scope.oCurrentSong = oSong;
+			});
+		};
+
+		$scope.setPercentage = function(lPercentage){
+			$scope.$apply(function(){
+				$scope.lPercentage = lPercentage;
+			});
+		};
+
+		$scope.setPlayCounter = function(lValue, bCount){
+			$scope.$apply(function(){
+				$scope.lPlayCounter = lValue;
+			});
+			$scope.setPercentage((lValue / $scope.oCurrentSong.lLength) * 100);
+			if($scope.oTimeout != null)
+				clearTimeout($scope.oTimeout);
+			if(!bCount)
+				return;
+			$scope.oTimeout = setTimeout(function(){
+				$scope.setPlayCounter(++lValue, bCount);
+			}, 1000);
+		};
+
+		$scope.isPlaying = function(){
+			return $scope.bPlay;
+		};
+
+		$scope.formatTime = function(lSeconds){
+			return Math.floor(lSeconds / 60) + ":" + (lSeconds % 60 < 10 ? "0" + lSeconds % 60 : lSeconds % 60);
+		};
+
 	});
 
 })();
+
+// -----------------------------------------------------------------------------
+// Socket Control
+//
+var oClient = new WebSocket("ws://localhost:1234");
+oClient.onopen = function(){
+	oClient.send(JSON.stringify({"type":"SongRequest","content":""}));
+	$("#next").click(function(){
+		oClient.send(JSON.stringify({"type":"PlayerCommand","content":"next"}));
+	});
+	$("#prev").click(function(){
+		oClient.send(JSON.stringify({"type":"PlayerCommand","content":"prev"}));
+	});
+	$("#play").click(function(){
+		if(oMediaScope.isPlaying()){
+			oClient.send(JSON.stringify({"type":"PlayerCommand","content":"pause"}));
+		}else{
+			oClient.send(JSON.stringify({"type":"PlayerCommand","content":"play"}));
+		}
+	});
+};
+oClient.onmessage = function(sMessage){
+	var oMessage = JSON.parse(sMessage.data);
+	if(oMessage.type == "SongChanged" || oMessage.type == "SongResponse" || oMessage.type == "PlayerStatusChanged"){
+		oMediaScope.setCurrentSong(oMessage.content);
+		if(typeof oMessage.player != "undefined"){
+			oPlayer = oMessage.player;
+			oMediaScope.setPlay(oMessage.player.bPlaying);
+			oMediaScope.setPlayCounter(oMessage.player.lCurrentTime, oMessage.player.bPlaying);
+		}
+	}
+}
